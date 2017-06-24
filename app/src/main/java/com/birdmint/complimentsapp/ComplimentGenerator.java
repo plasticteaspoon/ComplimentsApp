5d87309d70545a281fc58a83e496814fb5566522
plasticteaspoon <jessica@drurys.org>
2017-06-24 13:46:41 +0100

    Fixed the double space problem but not in a goood way. Needs correcting.

--- app/src/main/java/com/birdmint/complimentsapp/ComplimentGenerator.java
+++ app/src/main/java/com/birdmint/complimentsapp/ComplimentGenerator.java
@@ -1,61 +1,47 @@
 package com.birdmint.complimentsapp;
 
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.res.Resources;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 import com.birdmint.complimentsapp.R;
 
 /**
  * Created by jessi on 16/06/2017.
  */
 
-public class ComplimentGenerator extends ContextWrapper {
+public class ComplimentGenerator {
 
-    public ComplimentGenerator (Context ctx) {
-        super(ctx);
+    WordList bodyParts;
+    WordList verbs;
+    WordList adjectives;
+    private Random random;
+
+    public ComplimentGenerator(Context ctx) {
+        verbs = new WordList(ctx, R.array.verbs);
+        bodyParts = new WordList(ctx, R.array.bodyParts);
+        adjectives = new WordList(ctx, R.array.adjectives);
+
+        random = new Random();
     }
 
-    private Random random = new Random();
-    //private static List<String> compliments = null;
 
-    Resources res = getResources();
-
-    String[] bodyParts = res.getStringArray(R.array.bodyParts);
-    String[] verbs = res.getStringArray(R.array.verbs);
-    String[] adjectives = res.getStringArray(R.array.adjectives);
-
-    public String getCompliment () {
+    public String getCompliment() {
 
         String message = null;
 
-        /*if(compliments == null) {
-            compliments = new ArrayList<String>();
+        //you = 0 and your = 1
+        int youNum = random.nextInt(2);
 
-            compliments.add("You are looking wonderful today!");
-            compliments.add("Your hair looks stunning!");
-            compliments.add("Your skin is looking beautiful today!");
-        }
+        if (youNum == 0) {
+            message = "You are" + verbs.getWord() + adjectives.getWord() + ".";
 
-        if(compliments.size() != 0) {
-            int index = random.nextInt(compliments.size());
-            message = compliments.remove(index);
         } else {
-            message = "Sorry, but we're out of compliments for just now. Come back for more later.";
-        }*/
-
-        int bodyPartIndex = random.nextInt(bodyParts.length);
-        int verbIndex = random.nextInt(verbs.length);
-        int adjectiveIndex = random.nextInt(adjectives.length);
-
-        String bodyPart = bodyParts[bodyPartIndex];
-        String verb = verbs[verbIndex];
-        String adjective = adjectives[adjectiveIndex];
-
-        message = "Your " + bodyPart + " " + verb + " " + adjective + " today.";
+            message = "Your" + bodyParts.getWord() + verbs.getWord() + adjectives.getWord() + " today.";
+        }
 
         return message;
     }
---
+++ app/src/main/java/com/birdmint/complimentsapp/DisplayComplimentActivity.java
@@ -0,0 +1,34 @@
+package com.birdmint.complimentsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static android.R.id.message;
+
+public class DisplayComplimentActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compliments);
+
+        Intent intent = getIntent();
+
+        // create a compliment
+        ComplimentGenerator generator = new ComplimentGenerator(getApplicationContext());
+        String message = generator.getCompliment();
+
+        // display the compliment
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
