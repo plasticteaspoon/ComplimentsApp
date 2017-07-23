55ee0e0826721a86b8f0cae6e71ded8cf772cacc
plasticteaspoon <jessica@drurys.org>
2017-07-23 17:10:05 +0100

    Working on making it release ready

--- app/src/main/java/com/birdmint/complimentsapp/MainActivity.java
+++ app/src/main/java/com/birdmint/complimentsapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
